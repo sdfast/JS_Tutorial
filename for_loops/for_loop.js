2ef86db2e1d47099b3568fa26d00f29ac88c2375
sdfast <sdfast@o2.pl>
2018-11-05 16:52:44 +0100

    More Loops

--- for_loops/for_loop.js
+++ for_loops/for_loop.js
@@ -2,10 +2,20 @@ document.write("For_Loops")
 var a = "a"
 var word = "ABCDE"
 
-for (var i = 0; i<5;i++){
-  console.log("i = " + i);
+// for (var i = 0; i<5;i++){
+//   console.log("i = " + i);
+// }
+//
+// for (var i = 0; i < word.length; i=i+2){
+//   console.log(word[i]);
+// }
+
+for (var i = 0; i<5; i=i+1){
+  console.log("Hello World");
 }
 
-for (var i = 0; i < word.length; i=i+2){
-  console.log(word[i]);
+for (var i = 0; i < 25; i++) {
+  if (i%2 !== 0){
+    console.log(i);
+      }
 }
--- while_loops/while.js
+++ while_loops/while.js
@@ -1,22 +1,39 @@
-var x = 0;
+// var x = 0;
+//
+// while (x<5) {
+//   console.log("X is currently: " +x);
+//   if (x===3) {
+//     console.log("X is equal to 3");
+//     break;
+//   }
+//   document.write("X is still less than 5, adding 1 to X \n");
+//
+//   console.log("X is still less than 5, adding 1 to X");
+//   x = x + 1;
+// }
 
-while (x<5) {
-  console.log("X is currently: " +x);
-  if (x===3) {
-    console.log("X is equal to 3");
-    break;
+// y = 1
+//
+// while (y < 11) {
+//   if (y % 2 === 0){
+//   console.log(y);
+// }
+//   y = y + 1;
+// }
+//
+// var i = 0
+// while (i<5) {
+//   document.write("Hello World ");
+//   if (i === 4){
+//     break;
+//   }
+//   i = i+1;
+// }
+
+var num = 1;
+while (num < 25) {
+  if(num%2 !==0){
+    console.log(num);
   }
-  document.write("X is still less than 5, adding 1 to X \n");
-
-  console.log("X is still less than 5, adding 1 to X");
-  x = x + 1;
-}
-
-y = 1
-
-while (y < 11) {
-  if (y % 2 === 0){
-  console.log(y);
-}
-  y = y + 1;
+  num++
 }
