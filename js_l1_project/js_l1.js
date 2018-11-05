f5e48f0a4a9e6140f4c91a6a502bbdf9431345fd
sdfast <sdfast@o2.pl>
2018-11-05 17:26:51 +0100

    variables added

--- js_l1_project/js_l1.js
+++ js_l1_project/js_l1.js
@@ -1,2 +1,8 @@
 
 console.log("Javascript L1 tutorial project!");
+
+
+var first_name = (prompt("What is you first name?"))
+var last_name = (prompt("What is your last name?"))
+var height = (prompt("How tall are you in \"cm\""?))
+var pet_name = (prompt("What is the name of your pet?"))
