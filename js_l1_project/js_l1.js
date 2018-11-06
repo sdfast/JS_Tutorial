f05029c88985dc448b6c905f51422e208d35f247
sdfast <sdfast@o2.pl>
2018-11-06 17:09:47 +0100

    js project update

--- js_l1_project/js_l1.js
+++ js_l1_project/js_l1.js
@@ -1,8 +1,34 @@
 
-console.log("Javascript L1 tutorial project!");
+// console.log("Javascript L1 tutorial project!");
 
 
-var first_name = (prompt("What is you first name?"))
-var last_name = (prompt("What is your last name?"))
-var height = (prompt("How tall are you in \"cm\""?))
-var pet_name = (prompt("What is the name of your pet?"))
+var first_name = prompt("What is you first name?")
+console.log("First name is: " + first_name);
+var last_name = prompt("What is your last name?")
+console.log("Last name is: " + last_name);
+var age = prompt("What is your age?")
+console.log("Age is: " + age);
+var height = prompt("How tall are you in \"cm\" ?")
+console.log("Height is: " + height);
+var pet_name = prompt("What is the name of your pet?")
+console.log("Pet name is: " + pet_name);
+
+if (first_name[0] === last_name[0]){
+  console.log("Beginning of the First name and Last name is the same");
+} else {
+  console.log("Beginning of the First and Last name is different");
+}
+
+if (age > 20 && age <30){
+  console.log("Age between 20 or 30");
+} else {
+  "Age is different"
+}
+
+if (height >= 170){
+  console.log("Height us at least 170cm");
+} else {
+  console.log("Height is less than 170cm");
+}
+//
+// if first_name ==
