1e4f4bcd9064b4f00b265d15877a38a15f548e84
sdfast <sdfast@o2.pl>
2018-11-07 16:47:43 +0100

    Functions tutorial - completed

--- functions/func1.js
+++ functions/func1.js
@@ -13,10 +13,25 @@ function helloSbd (first_name = "Bobby", last_name = "Basingha") {
     alert(first_name + " " + last_name);
 }
 
-function addtion (x, y) {
-  return added = x + y,
-   alert("X + Y = " + (added));
-}
+// function addtion (x, y) {
+//   return added = x + y,
+//    alert("X + Y = " + (added));
+// }
+//
+// function timesFive(numInput){
+//   var result = numInput * 5
+//   return result
+// }
 
-addtion(1, 2)
-helloSbd()
+var v = "Global V"
+var stuff = "Global Stuff"
+
+function fun(stuff) {
+  console.log(v);
+  stuff = "Change stuff inside function"
+  console.log(stuff);
+}
+fun()
+console.log(stuff);
+// addtion(1, 2)
+// helloSbd()
