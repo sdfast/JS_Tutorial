a13be9af7ce18241d489b5b1c08dcfa5249c96fe
sdfast <sdfast@o2.pl>
2018-11-07 15:39:36 +0100

    Functions tutorial

---
+++ functions/func1.js
@@ -0,0 +1,22 @@
+var name = "Pawel"
+
+// function Hello (a) {
+//   alert(a + " " + name) , document.write(a), console.log(a);
+// }
+//
+// a = prompt("Say Hello: ")
+
+//func with default parameter
+function helloSbd (first_name = "Bobby", last_name = "Basingha") {
+  return full_name = (first_name + " " + last_name),
+   console.log(full_name),
+    alert(first_name + " " + last_name);
+}
+
+function addtion (x, y) {
+  return added = x + y,
+   alert("X + Y = " + (added));
+}
+
+addtion(1, 2)
+helloSbd()
