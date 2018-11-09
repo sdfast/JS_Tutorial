76c717975b3f8e5074250b5ea72261c50a62b6af
sdfast <sdfast@o2.pl>
2018-11-09 13:53:00 +0100

    js excercise progress

--- functions/functions_excercise1.js
+++ functions/functions_excercise1.js
@@ -1,4 +1,17 @@
 
+
 function sleepIn(weekday, vacation){
-  
+  return (!weekday || vacation)}
+
+function monkeyTrouble(aSmile, bSmile){
+  return (aSmile && bSmile) || (!aSmile && !bSmile)}
+
+function stringTimes(str, n){
+  var returnStr="";
+  var i = 0;
+  while (i<n){
+    returnStr += str
+    i++
+  }
+  return returnStr
 }
--- if_else_js/if_else.js
+++ if_else_js/if_else.js
@@ -1,15 +1,15 @@
-// var hot = false
-// var temp = prompt()
-//
-// if (temp >= 35){
-//   alert("it'svery hot today")
-// }
-// else if (temp >= 20){
-//   alert("it's quite hot today")
-// }
-// else if (temp <= 19){
-//   alert("it's not hot")
-// }
-// else{
-// alert("it's cold")
-//     };
+var hot = false
+var temp = prompt()
+
+if (temp >= 35){
+  alert("it'svery hot today")
+}
+else if (temp >= 20){
+  alert("it's quite hot today")
+}
+else if (temp <= 19){
+  alert("it's not hot")
+}
+else{
+alert("it's cold")
+    };
