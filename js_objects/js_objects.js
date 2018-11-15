354d929a5d9be95281f1d8c4fada845de200701d
sdfast <sdfast@o2.pl>
2018-11-15 15:41:29 +0100

    objects tutorial

--- js_objects/js_objects.js
+++ js_objects/js_objects.js
@@ -1,3 +1,20 @@
 
 var carInfo = {make: "Toyota", year:1990, model:"Camry"}
 var mixedObject = {a: "boyo", b: [1,2,3], c: {inside:carInfo}}
+
+
+for (key in mixedObject){
+  console.log(key);
+  console.log(mixedObject[key]);
+  console.log("elemnt displayed");
+}
+
+for (key in carInfo){
+  if (key === "model"){
+  console.log(key);
+  }
+}
+
+for (key in carInfo){
+  console.log(key, ":", carInfo[key]);
+}
