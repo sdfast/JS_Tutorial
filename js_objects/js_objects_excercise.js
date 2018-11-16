1a24592c61fb6f329f5519da21fcc0ce1816ccb0
sdfast <sdfast@o2.pl>
2018-11-16 16:06:42 +0100

    Please enter the commit message for your changes. Lines starting

--- js_objects/js_objects_excercise.js
+++ js_objects/js_objects_excercise.js
@@ -1,16 +1,30 @@
 console.log("start excercise");
 
 var employee = {
-  name: "John Smith",
+  name: "John Smith Brown",
   job: "Programmer",
   age: 31,
-  emplMethod: "empty"
+// function displays employee value lenght
+  nameLength: function(){
+    console.log(this.name + " object string length is: ");
+    console.log(this.name.length);
+    },
+  alertAll: function(){
+    alert("Name is " + this.name + ", Job is " + this.job +", Age is " + this.age)
+  },
+  lastName: function(){
+    console.log(this.name.split(" ")[0]);
+  }
 }
 
+employee.nameLength()
+employee.alertAll()
+employee.lastName()
+// employee.emplAllMethod()
+//
 
+// for (empl in employee == "name"){
+// console.log(employee[empl].length)
+// }
 
-// function(){
-//   for (key in employee){
-//     console.log(employee.emplMethod(key.length));
-//     }
-//   }
+console.log("End excercise");
