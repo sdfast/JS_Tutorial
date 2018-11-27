cfd0aad9eacee3143fa53677e913deabb71e504e
sdfast <sdfast@o2.pl>
2018-11-27 17:25:33 +0100

    DOM tutorial-2

---
+++ DOM/dom.js
@@ -0,0 +1,21 @@
+
+var myheader = document.querySelectorAll("h1")
+
+//random color generator
+// function getRandomColor(){
+//   var letters = "0123456789ABCDEF";
+//   var color = '#';
+//   for (var i = 0; i < 6; i++){
+//     color += letters[Math.floor(Math.random()*16)];
+//   }
+//   return color
+// }
+
+//function that applies color generator to elements
+// function changeHeaderColor(){
+//   colorInput = getRandomColor()
+//   myheader.style.color = colorInput;
+// }
+
+//performe action overt intervals (miliseconds)
+setInterval("changeHeaderColor()",500)
