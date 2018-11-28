b8d5a7623d36b4ccfabfc89f26384c3144e131fd
sdfast <sdfast@o2.pl>
2018-11-28 12:27:37 +0100

    DOM change color function fix update

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,21 +1,23 @@
 
-var myheader = document.querySelectorAll("h1")
+var myheader = document.querySelector("h1")
+var myheader.style.color = "red"
 
 //random color generator
-// function getRandomColor(){
-//   var letters = "0123456789ABCDEF";
-//   var color = '#';
-//   for (var i = 0; i < 6; i++){
-//     color += letters[Math.floor(Math.random()*16)];
-//   }
-//   return color
-// }
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = '#';
+  for (var i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color
+}
+
 
 //function that applies color generator to elements
-// function changeHeaderColor(){
-//   colorInput = getRandomColor()
-//   myheader.style.color = colorInput;
-// }
+function changeHeaderColor(){
+  colorInput = getRandomColor()
+  myheader.style.color = colorInput;
+}
 
 //performe action overt intervals (miliseconds)
 setInterval("changeHeaderColor()",500)
