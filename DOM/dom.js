2b16e7de83f71d8236efc3345011b869e1b51184
sdfast <sdfast@o2.pl>
2018-11-28 14:30:49 +0100

    more on DOM manipulation

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,6 +1,6 @@
 
-var myheader = document.querySelector("h1")
-var myheader.style.color = "red"
+var myheader = document.querySelectorAll("h1")
+
 
 //random color generator
 function getRandomColor(){
@@ -12,12 +12,24 @@ function getRandomColor(){
   return color
 }
 
+getRandomColor()
 
 //function that applies color generator to elements
 function changeHeaderColor(){
+  for (var i=0; i<2; i++){
   colorInput = getRandomColor()
-  myheader.style.color = colorInput;
+  myheader[i].style.color = colorInput;
+  }
 }
 
+changeHeaderColor()
 //performe action overt intervals (miliseconds)
-setInterval("changeHeaderColor()",500)
+setInterval("changeHeaderColor()",300)
+
+var parag = document.querySelectorAll("p")
+
+for (var p in parag){
+  parag[0].textContent = "XXX XXX XXX"
+  parag[1].textContent = "<strong>ZZZ ZZZ ZZZ</strong>"
+  parag[2].innerHTML = "<strong><i>CCC CCC CCC</i></strong>"
+}
