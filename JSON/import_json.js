24f8f7105706b0a3ee9fbb44e8589e3c2df09444
sdfast <sdfast@o2.pl>
2018-11-29 15:46:25 +0100

    creating  html elements from JSON

--- JSON/import_json.js
+++ JSON/import_json.js
@@ -1,9 +1,37 @@
-'use strict';
+var team = {
+  T1: "Proxy team",
+  T2: "Jinan team",
+  T3: "DevOps team"
+}
+team = JSON.stringify(team);
+team = JSON.parse(team)
 
-fetch("objects.json")
-  .then(function(resp){
-  return resp.json();
-})
-.then(function(data) {
-  createelement()
-})
+for (t in team){
+  document.write("teams: " + team[t] + " , ")
+}
+
+
+var employee = [
+  {
+    name: "Pawel",
+    age: 33
+  },
+  {
+    name: "Kacper",
+    age: 25
+  },
+  {
+    name: "Tomek",
+    age: 28
+  }
+];
+
+var output = "";
+for (var e = 0; e<3; e++){
+  document.write('<p id="op">' + employee[e].name + '</p>')
+  // console.log(output += '<li>'+ employee[e].name +'</li>');
+}
+
+// for (t in team){
+//   team[t].innerHTML
+// }
