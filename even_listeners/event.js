967dd46f2d4a22777780b81f5a2883a73f17017d
sdfast <sdfast@o2.pl>
2018-12-04 17:50:01 +0100

    event listeners update

--- even_listeners/event.js
+++ even_listeners/event.js
@@ -3,10 +3,20 @@ var headTwo = document.querySelector('#two')
 var headThree = document.querySelector('#three')
 
 headOne.addEventListener('mouseover',function(){
+  headOne.textContent = "Hover Over Me!"
   headOne.style.color = 'grey';
 })
 
 headOne.addEventListener('mouseout',function(){
   headOne.textContent = "Hover over me!"
   headOne.style.color = 'black'
+})
+
+headTwo.addEventListener('click',function(){
+  headTwo.style.color = 'blue'
+})
+
+
+headThree.addEventListener('dblclick',function(){
+  headThree.style.color = 'purple';
 })
