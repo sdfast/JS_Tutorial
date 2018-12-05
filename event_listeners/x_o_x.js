e19d3f8c8d78c817e137fab762b90caba716f2ca
sdfast <sdfast@o2.pl>
2018-12-05 16:48:48 +0100

    xox game function update

--- event_listeners/x_o_x.js
+++ event_listeners/x_o_x.js
@@ -1,1 +1,29 @@
 
+// reset button
+var restart = document.querySelector("#bt1")
+
+var squares = document.querySelectorAll('td')
+
+function clearBoard(){
+  for (var i = 0; i < squares.length; i++){
+  squares[i].textContent = "";
+  }
+}
+
+restart.addEventListener('click', clearBoard);
+
+function changeMarker(){
+    if(this.textContent === ''){
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    }else {
+      this.textContent = '';
+    }
+};
+
+
+for (var i =0; i<squares.length; i++){
+  squares[i].addEventListener('click',changeMarker)
+}
