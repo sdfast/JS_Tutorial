bae851c65b1ff40141606e4aba745ac25835a2aa
sdfast <sdfast@o2.pl>
2018-12-07 15:42:39 +0100

    update on JQuery

--- JQuery/jq.js
+++ JQuery/jq.js
@@ -1,13 +1,34 @@
 var newCss = {
     'color':'white',
     'background':'black',
-    'border':'blue'}
+    'border-style':'dotted',
+    'border-color':'red'}
 
 
 var elem_h1 = $('h1')
-var listItems = $('li')
 
 elem_h1.css(newCss)
+elem_h1.css('margin-top', '20px')
+
+var listItems = $('li')
+// .eq selects number of item's index
 listItems.css('color','pink')
 listItems.eq(0).css('color','red')
 listItems.eq(0).css('font-family','Courier New')
+
+var inp = $('input')
+
+inp.eq(0).val('new value!')
+
+// adding a class to an element
+$('h1').addClass('changed1')
+$('li').addClass('all_lists')
+// removing a class$
+
+$('li').removeClass('all_lists')
+
+var parag = $('p')
+
+// for (var i=0; i<2, i++){
+//   parag.toggleClass('turnBlue')
+// }
