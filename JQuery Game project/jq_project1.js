ec026a12d98a47a87f3a7577fa801200a394d796
sdfast <sdfast@o2.pl>
2019-01-08 17:14:54 +0100

    update

--- JQuery Game project/jq_project1.js
+++ JQuery Game project/jq_project1.js
@@ -1,70 +1,21 @@
-var newCss = {
-    'color':'white',
-    'background':'black',
-    'border-style':'dotted',
-    'border-color':'red'}
+var player1 = prompt('Player One: Enter your name, you will be Blue');
+// color has to be in rgb format for jQuery
+var player1Color = 'rgb(86, 151, 255)';
 
+var player2 = prompt('Player One: Enter your name, you will be Blue')
+var player2Color = 'rgb(237, 45, 73)';
 
-var elem_h1 = $('h1')
+// boolea var that tell whether the game is running or not
+var game_on = true;
+var table = $('table tr');
 
-elem_h1.css(newCss)
-elem_h1.css('margin-top', '20px')
+function reportWin(rowNum, colNum){
+  console.log("You won starting at this row, col");
+  console.log(rowNum);
+  console.log(colNum);
+}
 
-var listItems = $('li')
-// .eq selects number of item's index
-listItems.css('color','pink')
-listItems.eq(0).css('color','red')
-listItems.eq(0).css('font-family','Courier New')
+function changeColor(rowIndex,colIndex,color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
 
-var inp = $('input')
-
-inp.eq(0).val('new value!')
-
-// adding a class to an element
-$('h1').addClass('changed1')
-$('li').addClass('all_lists')
-$('p').addClass('turnRed')
-
-$('p').removeClass('turnRed')
-// removing a class$
-
-$('li').removeClass('all_lists')
-
-var parag = $('p')
-
-//Events
-
-// $('h1').click(function(){
-//   $(this).text("Changed Changed")
-// })
-
-
-
-function appendp1(){
-  return body.appendChild(p1)}
-
-
-$('li').click(function(){
-  console.log("<li>Was clicked");
-})
-
-$('input').eq(0).keypress(function(){
-  console.log(event);
-  if (event.code === "Enter"){
-    $('h3').toggleClass('turnBlue');
-  }
-})
-
-// on is like addEventListener
-$('p').on('dblclick',function() {
-  $(this).toggleClass('turnBlue');
-})
-
-// when input clicked "container class" is faded out after 3000
-$('input').eq(1).on('click', function(){
-  $('.container').fadeOut(3000)
-})
-
-$('input').eq(3).on('dblclick', function(){
-  $('.container').slideUp(2000)
-})
+}
