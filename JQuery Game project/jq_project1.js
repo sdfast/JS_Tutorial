000598237919ba0c7fbd44ec0502d2a4b5f5b691
sdfast <sdfast@o2.pl>
2019-01-16 16:53:28 +0100

    check functions

--- JQuery Game project/jq_project1.js
+++ JQuery Game project/jq_project1.js
@@ -36,5 +36,56 @@ function checkBottom(colIndex) {
   }
 }
 function colorMatchCheck(one, two, three, four){
-  return (one === two & one === three && one === four && and one !== 'rgb(128, 128, 128)')
+  return (one === two & one === three && one === four
+    && one !== 'rgb(128, 128, 128)' && one !== undefined)
+}
+
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (var col = 0; col < 5; col++) {
+    for (var row = 0; row < 7; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
 }
